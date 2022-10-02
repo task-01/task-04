61ba1f76ea9e446fa00360b4ca266e42d66bb0aa
correctionjs1-3 <tana39754@gmail.com>
2022-10-02 20:42:20 +0900

    1.改行コードの削除2.Enterでの改行とctrl+D押下による内容確定の修正、ご確認お願いします

--- hello.rb
+++ hello.rb
@@ -7,17 +7,21 @@ memo_type = gets.to_i # ユーザーの入力値を取得し、数字へ変換�
 if memo_type == 1
  puts "拡張子を除いたファイルを入力してください"
  file_name = gets.to_s
+ text = file_name.chomp
  puts "メモしたい内容を記入してください"
  puts "完了したらCtrl + Dをおします"
- file1 = File.open("#{file_name}.csv", "w") do |csv|
- file_content = gets.to_s
+ file1 = File.open("#{text}.csv", "w") do |csv|
+ file_content = STDIN.read
  csv << file_content
  end
 elsif memo_type == 2
  puts "編集したいfile名を入力してください、存在しない場合は新規で作成されます。"
  file_name2 = gets.to_s
- file2 = File.open("#{file_name2}.csv", "a") do |csv|
- file_content2 = gets.to_s
+ text2 = file_name2.chomp
+ puts "メモしたい内容を記入してください"
+ puts "完了したらCtrl + Dをおします"
+ file2 = File.open("#{text2}.csv", "a") do |csv|
+ file_content2 = STDIN.read
  csv << file_content2
  end
 else
